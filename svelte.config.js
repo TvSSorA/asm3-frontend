1d84dccb66849194c44ab79d5828ed1b201c2cb1
SorA <95413012+TvSSorA@users.noreply.github.com>
2025-01-03 20:09:52 +0700

    chore: configure `SCSS`

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,16 @@
 import adapter from "@sveltejs/adapter-node";
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { sveltePreprocess } from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: sveltePreprocess({
+		scss: {
+			prependData: `@use 'src/variables.scss' as *;`
+		}
+	}),
 
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
