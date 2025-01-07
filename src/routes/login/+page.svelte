24551e96cbd161f89447c531e0ba84f8bca4b3d1
SorA <95413012+TvSSorA@users.noreply.github.com>
2025-01-07 22:53:47 +0700

    feat: primitive `/login` markup

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,51 @@
+<div class="login
+    flex justify-center
+    py-20
+">
+    <div class="basic-details
+        card
+        bg-base-100
+        w-2/5
+        shadow-xl
+    ">
+        <div class="card-body
+            flex flex-col gap-4
+        ">
+            <h2 class="card-title">Login</h2>
+
+            <label class="form-control w-full">
+                <div class="label">
+                    <span class="label-text">Username</span>
+                </div>
+    
+                <input 
+                    type="text"
+                    placeholder="Username"
+                    class="input input-bordered w-full"
+                />
+            </label>
+    
+            <label class="form-control w-full">
+                <div class="label">
+                    <span class="label-text">Password</span>
+                </div>
+    
+                <input 
+                    type="text"
+                    placeholder="Password"
+                    class="input input-bordered w-full"
+                />
+            </label>
+
+            <p class="text-center">
+                Don't have an account yet? <a href="/signup" class="text-primary hover:underline">Sign Up</a>
+            </p>
+
+            <div class="card-actions justify-end">
+                <button class="btn btn-primary">
+                    Login
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
