680663c3caf37d13f44bd19ebba2c22811e128e2
SorA <95413012+TvSSorA@users.noreply.github.com>
2025-01-08 21:32:53 +0700

    feat: primitive `/signup` markup

---
+++ src/routes/signup/+page.svelte
@@ -0,0 +1,63 @@
+<div class="signup
+    flex justify-center
+    py-20
+">
+    <div class="basic-details
+        card
+        bg-base-100
+        w-2/5
+        shadow-xl
+    ">
+        <div class="card-body
+            flex flex-col gap-4
+        ">
+            <h2 class="card-title">Sign Up</h2>
+
+            <label class="form-control w-full">
+                <div class="label">
+                    <span class="label-text">Username</span>
+                </div>
+    
+                <input 
+                    type="text"
+                    placeholder="Username"
+                    class="input input-bordered w-full"
+                />
+            </label>
+    
+            <label class="form-control w-full">
+                <div class="label">
+                    <span class="label-text">Password</span>
+                </div>
+    
+                <input 
+                    type="text"
+                    placeholder="Password"
+                    class="input input-bordered w-full"
+                />
+            </label>
+
+            <label class="form-control w-full">
+                <div class="label">
+                    <span class="label-text">Confirm Password</span>
+                </div>
+    
+                <input 
+                    type="text"
+                    placeholder="Confirm Password"
+                    class="input input-bordered w-full"
+                />
+            </label>
+
+            <p class="text-center">
+                Already have an account? <a href="/login" class="text-primary hover:underline">Login</a>
+            </p>
+
+            <div class="card-actions justify-end">
+                <button class="btn btn-primary">
+                    Sign Up
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
