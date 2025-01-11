0b6821f326237a35be8e78751ae1ca9f36f96ad0
SorA <95413012+TvSSorA@users.noreply.github.com>
2025-01-11 11:37:14 +0700

    feat: integrate `DELETE /api/post`

--- src/routes/DeleteJobModal.svelte
+++ src/routes/DeleteJobModal.svelte
@@ -1,3 +1,27 @@
+<script lang="ts">
+    let { title, username }: { title: string, username: string } = $props();
+
+    let message: string = $state("");
+
+    async function delete_job() {
+        const res = await fetch('/api/post', {
+            method: 'DELETE',
+            body: JSON.stringify({
+                title,
+                company: username
+            })
+        });
+        const body = await res.json();
+
+        if (res.status === 200) {
+            window.location.href = '/';
+        }   
+        else {
+            message = `Error: ${res.status} ${res.statusText} - ${body.message}`;
+        }
+    }
+</script>
+
 <!-- svelte-ignore a11y_consider_explicit_label -->
 <button class="btn btn-circle btn-sm btn-error" onclick={() => document.getElementById('delete_job_modal')?.showModal()}>
     <svg
@@ -20,8 +44,11 @@
     <div class="modal-box">
         <h3 class="text-lg font-bold">Are you really sure?</h3>
         <p class="py-4">This will <strong>completely</strong> delete this job post.</p>
+
+        <h1 class="text-center text-error">{message}</h1>
+
         <div class="modal-action">
-            <button class="btn btn-error">DELETE JOB</button>
+            <button class="btn btn-error" onclick={delete_job}>DELETE JOB</button>
         </div>
     </div>
     
