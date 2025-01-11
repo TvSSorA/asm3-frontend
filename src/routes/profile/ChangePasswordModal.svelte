ef0a930dad5906d290522f765bc0fbe78962e2c6
SorA <95413012+TvSSorA@users.noreply.github.com>
2025-01-11 11:35:48 +0700

    feat: integrate `PUT /api/user`

--- src/routes/profile/ChangePasswordModal.svelte
+++ src/routes/profile/ChangePasswordModal.svelte
@@ -1,3 +1,39 @@
+<script lang="ts">
+    let { username }: { username: string } = $props();
+
+    let password: string = $state('');
+    let confirm_password: string = $state('');
+
+    let error: boolean = $state(false);
+    let message: string = $state('');
+
+    async function change_password() {
+        if (password !== confirm_password) {
+            message = 'Passwords do not match';
+            error = true;
+            return;
+        }
+
+        const res = await fetch('/api/user', {
+            method: 'PUT',
+            body: JSON.stringify({
+                username,
+                password
+            })
+        });
+        const body = await res.json();
+
+        if (res.status === 200) {
+            message = 'Password successfully changed!';
+            error = false;
+        }   
+        else {
+            message = `Error: ${res.status} ${res.statusText} - ${body.message}`;
+            error = true;
+        }
+    }
+</script>
+
 <button class="btn btn-warning" onclick={() => document.getElementById('change_password_modal')?.showModal()}>
     Change Password
 </button>
@@ -14,6 +50,7 @@
                 type="password"
                 placeholder="New Password"
                 class="input input-bordered w-full"
+                bind:value={password}
             />
         </label>
 
@@ -26,11 +63,14 @@
                 type="password"
                 placeholder="Confirm Password"
                 class="input input-bordered w-full"
+                bind:value={confirm_password}
             />
         </label>
 
+        <h1 class="text-center {error ? "text-error" : "text-success"}">{message}</h1>
+
         <div class="modal-action">
-            <button class="btn btn-warning">Change Password</button>
+            <button class="btn btn-warning" onclick={change_password}>Change Password</button>
         </div>
     </div>
     
