f09097051f24d630fa07506f50d6a6fb233a2d3a
SorA <95413012+TvSSorA@users.noreply.github.com>
2025-01-12 10:47:50 +0700

    fix: replace unused `nav` links with `/`

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -6,25 +6,10 @@
 
 <header class="bg-neutral p-4">
 	<nav class="
-		flex flex-row justify-between
+		flex flex-row justify-between items-center
 		text-base-300
 	">
-		<div class="logo-and-links
-			flex flex-row items-center gap-8
-		">
-			<div class="logo w-24">
-				<a href="/">
-					<img
-						src="https://itviec.com/assets/logo-itviec-4492a2f2577a15a0a1d55444c21c0fa55810822b3b189fc689b450fb62ce0b5b.png"
-						alt="Logo"
-					>
-				</a>
-			</div>
-	
-			<div class="links">
-				<a href="/jobs" class="hover:text-base-100 transition">IT Jobs</a>
-			</div>
-		</div>
+		<a href="/" class="hover:text-base-100 transition">Home</a>
 	
 		{#if user.username}
 			<a href="/profile">
