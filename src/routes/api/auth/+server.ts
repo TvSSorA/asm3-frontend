65a31f883a8a9a2c62e80557d2b348e7fd1f2e40
SorA <95413012+TvSSorA@users.noreply.github.com>
2025-01-12 10:12:28 +0700

    feat: define authentication endpoint `/api/auth`

---
+++ src/routes/api/auth/+server.ts
@@ -0,0 +1,34 @@
+import { PUBLIC_DB_URL } from "$env/static/public";
+import { exception_handler } from "$lib/exception_handler";
+import type { RequestHandler } from "./$types";
+
+export const POST: RequestHandler = async ({ request }) => {
+    const body: User = await request.json();
+    const { username, password } = body;
+
+    if (!username || !password) return exception_handler(400, "Bad Request", "Missing required fields");
+
+    const res = await fetch(`${PUBLIC_DB_URL}/auth`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            username,
+            password
+        })
+    });
+    const data = await res.json();
+
+    if (data.error) {
+        return exception_handler(500, "Internal Server Error", "Unexpected Server Error");
+    }
+
+    return new Response(JSON.stringify(data), {
+        status: 201,
+        statusText: "OK",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+}
